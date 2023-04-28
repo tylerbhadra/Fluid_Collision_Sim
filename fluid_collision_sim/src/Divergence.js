ada5012824d54639b567e6eeb00fb529f33c9e89
kaitalyn <github@charle.cc>
2023-04-28 16:58:41 -0700

    Add Divergence, Gradient, and Jacobi

---
+++ fluid_collision_sim/src/Divergence.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+
+/** 
+ *  Takes in a texture and computes its divergence
+ */
+export default class Divergence {
+    constructor(res) {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
+        this.gridRes = res;
+
+        this.uniforms = {
+            w: {type: 't', value: null},      // vector field
+
+            gridSize: {type: 'v2', value: this.gridRes},
+            gridScale: {type: 'f', value: 1.0},
+          }
+
+        this.material = new THREE.ShaderMaterial({
+            uniforms: this.uniforms,
+            fragmentShader: document.getElementById( 'divergenceFrag' ).innerHTML,
+            depthWrite: false,
+            depthTest: false,
+            blending: THREE.NoBlending
+        })
+
+        this.geometry = new THREE.PlaneGeometry( 2, 2 );
+        this.plane = new THREE.Mesh(this.geometry, this.material);
+        this.scene.add(this.plane);
+    }
+
+    jacobi_texture(renderer, w, output) {
+        this.renderer = renderer;
+        this.uniforms.w = w;
+  
+        this.renderer.setRenderTarget(output);
+        this.renderer.render(this.scene, this.camera);
+        this.renderer.setRenderTarget(null);
+    }
+}
