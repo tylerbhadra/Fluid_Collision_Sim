840391f21a734d0be9163a0056e93b8f248d6d31
Maxwell Lo <lo-maxwell@berkeley.edu>
2023-04-30 13:25:58 -0700

    framework for external forces

---
+++ fluid_collision_sim/src/External-Forces.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+
+/** 
+ *  Applies an external force to the fluid (based on mouse position and duration of click).
+ */
+export default class ExternalForce {
+    constructor(res) {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
+        this.gridRes = res;
+        this.source = new THREE.Vector3(0,0,0);
+        this.sourceValue = new THREE.Vector2(0,0);
+
+        this.uniforms = {
+            inputTexture: {type: 't', value: null},
+            source: {type: 'v3'},
+            sourceValue: {type: 'v2'},
+            gridSize: {type: 'v2', value: this.gridRes},
+            mousePress: {type: 'v2', value: null},
+            radius: {type: 'f', value: null},
+          }
+
+        this.material = new THREE.ShaderMaterial({
+            uniforms: this.uniforms,
+            fragmentShader: document.getElementById( 'externalForceFrag' ).innerHTML,
+            depthWrite: false,
+            depthTest: false,
+            blending: THREE.NoBlending
+        })
+
+        this.geometry = new THREE.PlaneGeometry( 2, 2 );
+        this.plane = new THREE.Mesh(this.geometry, this.material);
+        this.scene.add(this.plane);
+    }
+
+    apply_force(renderer, input, radius, output) {
+        this.renderer = renderer;
+        this.uniforms.inputTexture.value = input.texture;
+        this.uniforms.radius.value = radius;
+        this.renderer.setRenderTarget(output);
+        this.renderer.render(this.scene, this.camera);
+        this.renderer.setRenderTarget(null);
+    }
+}
